16249f621af5cc8ce264388b1c9c8f44a238abf4
sonana <4775614@naver.com>
2022-10-05 06:25:00 +0900

    ✨Feat: comment Get, Posting 기능 구현

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,11 +3,11 @@ import './Modal.scss';
 import { CgClose } from 'react-icons/cg';
 import Comment from '../comment/Comment';
 
-const Modal = ({ setModalOpen, item }) => {
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-  return (
+const Modal = ({ setModalOpen, item, rollingPaperId }) => {
+	const closeModal = () => {
+		setModalOpen(false);
+	};
+	return (
 		<div className="modal_modal">
 			<div className="modal_wrap">
 				<div className="modal_container">
@@ -15,11 +15,9 @@ const Modal = ({ setModalOpen, item }) => {
 					<div className="modal_letter_wrap">
 						<div className="modal_name_emoji">👑</div>
 						<div className="modal_name">👉🏻 {item.name} 👈🏻</div>
-						<div className="modal_content">
-							{item.content}
-						</div>
+						<div className="modal_content">{item.content}</div>
 					</div>
-					<Comment />
+					<Comment rollingPaperId={rollingPaperId} />
 				</div>
 			</div>
 		</div>
--- src/redux/modules/paperSlice.js
+++ src/redux/modules/paperSlice.js
@@ -2,118 +2,163 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import instance from './instance';
 
 export const postingLetter = createAsyncThunk(
-  'postingLetter',
-  async (payload, thunkAPI) => {
-    try {
-      const response = await instance.post(`/rolling-paper`, payload, {
-        withCredentials: true,
-      });
-      //  return console.log(response);
-      if (response.data.success === true) {
-        return thunkAPI.fulfillWithValue(response.data.result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	'postingLetter',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.post(`/rolling-paper`, payload, {
+				withCredentials: true,
+			});
+			//  return console.log(response);
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 );
 
 export const getLetter = createAsyncThunk(
-  'getLetter',
-  async (payload, thunkAPI) => {
-    try {
-      const response = await instance.get(`/rolling-paper`);
-      if (response.data.success === true) {
-        return thunkAPI.fulfillWithValue(response.data.result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	'getLetter',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.get(`/rolling-paper`);
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 );
 
 export const patchLetter = createAsyncThunk(
-  'patchLetter',
-  async (payload, thunkAPI) => {
-    try {
-      const response = await instance.patch(
-        `/rolling-paper/${payload.rollingPaperId}`,
-        {
-          "top": payload.position.y,
-          "left": payload.position.x,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-      console.log(response)
-      if (response.data.success === true) {
-        return thunkAPI.fulfillWithValue(response.data.result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	'patchLetter',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.patch(
+				`/rolling-paper/${payload.rollingPaperId}`,
+				{
+					top: payload.position.y,
+					left: payload.position.x,
+				},
+				{
+					withCredentials: true,
+				}
+			);
+			console.log(response);
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 );
 
 //롤링페이퍼 작성 Post - ("/rolling-paper")
 export const addReview = createAsyncThunk(
-  'addPaper',
-  async (payload, thunkAPI) => {
-    try {
-      const response = await instance.post(`/rolling-paper`, payload, {
-        //2. request리스트 그대로 적어주기
-        name: payload.name,
-        content: payload.content,
-      });
-      console.log(response); // 아래 response.data.result 자리에 무엇이 들어갈지 알 수 있음
-      if (response.data.success === true) {
-        return thunkAPI.fulfillWithValue(response.data.result);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	'addPaper',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.post(`/rolling-paper`, payload, {
+				//2. request리스트 그대로 적어주기
+				name: payload.name,
+				content: payload.content,
+			});
+			console.log(response); // 아래 response.data.result 자리에 무엇이 들어갈지 알 수 있음
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 );
 
 //롤링페이퍼 전체 조회 Get - ("/rolling-paper")
 //롤링페이퍼 상세 조회 Get - ("/rolling-paper/{rollingPaperId}")
 
 //댓글 작성 Post - ("/comments")
+export const postingComment = createAsyncThunk(
+	'postingComment',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.post(
+				`/comments`,
+				{
+					rollingPaperId: payload.rollingPaperId,
+					nickname: payload.data.nickname,
+					comment: payload.data.comment,
+				},
+				{
+					withCredentials: true,
+				}
+			);
+			//  return console.log(response);
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+);
+
 //롤링페이퍼마다 댓글 조회 Get - ("/comments/{rollingPaperId}")
+export const getComment = createAsyncThunk(
+	'getComment',
+	async (payload, thunkAPI) => {
+		try {
+			const response = await instance.get(`/comments/${payload}`);
+			if (response.data.success === true) {
+				return thunkAPI.fulfillWithValue(response.data.result);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+);
 
 const initialState = {
-  paper: [],
-  error: null,
+	paper: [],
+	review: [],
+	error: null,
 };
 
 export const paperSlice = createSlice({
-  name: 'paperSlice',
-  initialState,
-  reducers: {
-    onSaveHandler: (state, action) => {
-      state.paper = [...state.paper, action.payload];
-    },
-  },
-  extraReducers: {
-    [postingLetter.fulfilled]: (state, action) => {
-      state.paper = [...state.paper, action.payload];
-    },
-    [getLetter.fulfilled]: (state, action) => {
-      state.paper = action.payload;
-    },
-    [patchLetter.fulfilled]: (state, action) => {
-      state.paper.map((paper) => {
-        if (paper.id === action.payload.id) {
-          return (
-            (paper.left = action.payload.left), (paper.top = action.payload.top)
-          );
-        } else {
-          return paper;
-        }
-      });
-    },
-  },
+	name: 'paperSlice',
+	initialState,
+	reducers: {
+		onSaveHandler: (state, action) => {
+			state.paper = [...state.paper, action.payload];
+		},
+	},
+	extraReducers: {
+		[postingLetter.fulfilled]: (state, action) => {
+			state.paper = [...state.paper, action.payload];
+		},
+		[getLetter.fulfilled]: (state, action) => {
+			state.paper = action.payload;
+		},
+		[postingComment.fulfilled]: (state, action) => {
+			state.review = [...state.review, action.payload];
+		},
+		[getComment.fulfilled]: (state, action) => {
+			state.review = action.payload;
+		},
+		[patchLetter.fulfilled]: (state, action) => {
+			state.paper.map((paper) => {
+				if (paper.id === action.payload.id) {
+					return (
+						(paper.left = action.payload.left), (paper.top = action.payload.top)
+					);
+				} else {
+					return paper;
+				}
+			});
+		},
+	},
 });
 
 export const { onSaveHandler } = paperSlice.actions;
