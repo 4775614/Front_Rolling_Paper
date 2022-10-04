e809ca5e55a287b495b3d8270382e881cfe344f2
Yoonwon <blacksyw@naver.com>
2022-10-05 02:02:57 +0900

    🚧WIP: 모달 적용 중
    🚧WIP: 모달 적용 중

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,7 +3,7 @@ import './Modal.scss';
 import { CgClose } from 'react-icons/cg';
 import Comment from '../comment/Comment';
 
-const Modal = ({ setModalOpen }) => {
+const Modal = ({ setModalOpen, item }) => {
   const closeModal = () => {
     setModalOpen(false);
   };
@@ -14,10 +14,9 @@ const Modal = ({ setModalOpen }) => {
 					<CgClose onClick={closeModal} className="modal_close_icon" />
 					<div className="modal_letter_wrap">
 						<div className="modal_name_emoji">👑</div>
-						<div className="modal_name">👉🏻 항해8반짱 👈🏻</div>
+						<div className="modal_name">👉🏻 {item.name} 👈🏻</div>
 						<div className="modal_content">
-							8반이짱이야 1조는 더짱이야 하하하ㅏ하핳하하아인냥하세여 안녕하세요
-							이게 어떻게 들어가려낭 홍홍홍
+							{item.content}
 						</div>
 					</div>
 					<Comment />
